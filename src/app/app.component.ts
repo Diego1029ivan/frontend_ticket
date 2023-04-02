6f2edf57d49220e2766c8fc5727e1467d0fcf7a5
EsauProgramando <eaveintemillap@alumno.unsm.edu.pe>
2023-04-02 02:55:29 -0500

    mostrando datos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Inventario } from './interfaces/inventario';
+import { InventarioService } from './services/inventario.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  public urlCodigoBarra: string =
+    'http://localhost/backend_ticket/inventarioBarra';
+  constructor(private inventarioService: InventarioService) {}
+  inventarios: Inventario[] = [];
+  ngOnInit(): void {
+    this.inventarioService.getInventario().subscribe((inventario) => {
+      this.inventarios = inventario;
+    });
+    //mostrar el inventario por codigo de barras
+  }
   title = 'front_ticket';
+  mostrarCodigoBarra(idCogoBarra: string) {}
 }
