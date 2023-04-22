dd1754391b37ebe229fcd58d658357d77a61485e
Diego Alvarado <dialvaradov@alumno.unsm.edu.pe>
2023-04-22 08:49:36 -0500

    paquete

---
+++ src/app/interfaces/itemsSelect.ts
@@ -0,0 +1,10 @@
+export class ItemsSelect {
+    
+    item0?: string = '';
+    item1?: string = '';
+    item2?: string = '';
+    item3?: string = '';
+    item4?: string = '';
+    
+  }
+  
--- src/app/pages/filtro/filtro.component.ts
+++ src/app/pages/filtro/filtro.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ItemsSelect } from 'src/app/interfaces/itemsSelect';
 import { FiltroPipe } from 'src/app/pipes/filtro.pipe';
 
 import { InventarioService } from 'src/app/services/inventario.service';
@@ -33,19 +34,23 @@ export class FiltroComponent implements OnInit{
   constructor(
     private inventarioService: InventarioService,
     private http: HttpClient,
-    private filtro: FiltroPipe
-  ) {}
+    private filtro: FiltroPipe,
+    
+  ) {
+    
+  }
 
   ngOnInit(): void {
     this.inventarioService.getBienes().subscribe((respo)=>{
       this.items=respo
       this.cargaTabla()
+      
     });
     
   }
 
   public cargaTabla() {
-    console.log(this.items)
+    //console.log(this.items)
     this.header = Object.keys(this.items[0]);
     this.collectionSize = this.items.length;
     this.itemParcial = this.items;
@@ -89,6 +94,63 @@ export class FiltroComponent implements OnInit{
     console.log(this.busqueda, this.tablaFiltro)
     
   }
-  
 
+
+  /*=========CheckBox=============*/
+  maxSelected = 5;
+  selectedCount=0;
+  arregloSelect:any[]=[]
+  jsonSelect:ItemsSelect={
+
+  }
+  
+  getSelectedCount() {
+    const checkboxes = document.querySelectorAll('table input[type="checkbox"]');
+    let count = 0;
+    checkboxes.forEach((checkbox: any) => {
+      if (checkbox.checked === true) {
+        count++;
+      }
+      this.selectedCount=count
+    });
+    console.log(this.selectedCount) ;
+  }
+  disableCheckboxes() {
+    const checkboxes = document.querySelectorAll('table input[type="checkbox"]');
+    checkboxes.forEach((checkbox: any) => {
+      if (this.selectedCount >= this.maxSelected && checkbox.checked === false) {
+        checkbox.disabled = true;
+      } else {
+        checkbox.disabled = false;
+      }
+    });
+  }
+
+  imprimirPaquete(){
+    const checkboxes = document.querySelectorAll('table input[type="checkbox"]');
+    this.arregloSelect.splice(0,this.arregloSelect.length)
+    this.jsonSelect={}
+    checkboxes.forEach((checkbox: any) => {
+      if (checkbox.checked === true) {
+        this.arregloSelect.push(checkbox.value)
+      } 
+    });
+    //equivalencia con nombre de variables
+    for (let i = 0; i < this.maxSelected; i++) {
+      if(!this.arregloSelect[i]){
+        this.arregloSelect[i]=null;
+      }
+      eval('this.jsonSelect.item'+i+'= this.arregloSelect['+i+']'); 
+      
+    }
+    
+    
+    this.inventarioService.postpaqueteCodigo(this.jsonSelect).subscribe(
+      (pdf: Blob) => {
+      const fileUrl = URL.createObjectURL(pdf);
+      window.open(fileUrl);
+    })
+    
+  }
+  
 }
