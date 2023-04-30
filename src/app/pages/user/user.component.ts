7d6a4b3702a1e8537ca47420e9607a021cd3b78b
EsauProgramando <eaveintemillap@alumno.unsm.edu.pe>
2023-04-30 16:23:39 -0500

    poniendo token http

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
 })
 export class UserComponent {
-
+  constructor(private userService: UserService) {
+    this.userService.getAllUsers().subscribe((data) => {
+      console.log(data);
+    });
+  }
 }
