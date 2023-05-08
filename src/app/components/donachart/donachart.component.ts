d4523899a41f1e8de629dddf9d691c4956ed7e0e
Diego Alvarado <dialvaradov@alumno.unsm.edu.pe>
2023-05-07 22:16:27 -0500

    reporte

--- src/app/components/donachart/donachart.component.ts
+++ src/app/components/donachart/donachart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import Chart from 'chart.js/auto';
+import { InventarioService } from 'src/app/services/inventario.service';
 @Component({
   selector: 'app-donachart',
   templateUrl: './donachart.component.html',
@@ -9,50 +10,59 @@ export class DonachartComponent implements AfterViewInit, OnInit{
   @ViewChild('pieChart', { static: true })
   pieChart!: ElementRef<HTMLCanvasElement>;
 
-  constructor() {}
+  constructor(private inventarioServices:InventarioService) {}
   ngOnInit(): void {
-    const ctx = this.pieChart.nativeElement.getContext('2d');
-    if (!ctx) {
-      throw new Error('Error al obtener el contexto del canvas');
-    }
-    const myChart = new Chart(ctx, {
-    //type: 'bar',
-    type: 'doughnut',
-      data: {
-        labels: ['Red', 'Blue', 'Yellow'],
-        datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)'
-          ],
-          borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)'
-          ],
-          borderWidth: 1
-        }]
-      },
-      
-      options: {
-        responsive: true,
-        plugins: {
-          legend: {
-            position: 'top',
-            
-          },
-          
-          title: {
-            display: true,
-            text: 'Gráfico de Dona'
+    this.inventarioServices.getEstado().subscribe((estado)=>{
+      //console.log("condiciones",estado)
+    
+          const ctx = this.pieChart.nativeElement.getContext('2d');
+          if (!ctx) {
+            throw new Error('Error al obtener el contexto del canvas');
           }
-        }
-      }
-  
-  });
+          const myChart = new Chart(ctx, {
+          //type: 'bar',
+          type: 'doughnut',
+            data: {
+              labels: ['Bueno', 'Malo', 'Regular','Nuevo'],
+              datasets: [{
+                label: '# de bienes',
+                data: [parseInt(estado[0].cant_bueno),
+                       parseInt(estado[0].cant_malo),
+                       parseInt(estado[0].cant_regular),
+                       parseInt(estado[0].cant_nuevo)],
+                backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(120, 100, 86, 0.2)'
+                ],
+                borderColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(120, 100, 86, 1)'
+                ],
+                borderWidth: 1
+              }]
+            },
+            
+            options: {
+              responsive: true,
+              plugins: {
+                legend: {
+                  position: 'top',
+                  
+                },
+                
+                title: {
+                  display: true,
+                  text: 'Gráfico de Dona'
+                }
+              }
+            }
+        
+        });
+      })
   }
 
   ngAfterViewInit() {
