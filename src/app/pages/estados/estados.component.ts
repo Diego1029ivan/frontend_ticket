3a69d4f67283f7e3fbfc6d992caf1a867a083381
Diego Alvarado <dialvaradov@alumno.unsm.edu.pe>
2023-05-16 11:39:47 -0500

    modificar estado

--- src/app/pages/estados/estados.component.ts
+++ src/app/pages/estados/estados.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Inventario } from 'src/app/interfaces/inventario';
 import { InventarioService } from 'src/app/services/inventario.service';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-estados',
@@ -29,6 +30,21 @@ export class EstadosComponent {
     condicion: "A"
 
   }
+  /*=======FORMULARIO==========*/
+  formulario = {
+    codigo_patrimonial: '',
+    estado_bien1: '',
+    estado_bien2: '',
+    condicion1:'',
+    condicion2: ''
+
+  };
+
+  enviarData={
+    codigo_patrimonial: '',
+    estado_bien: '',
+    condicion: '',
+  }
   descBien:any[]=[]
   constructor(private router:Router,
     private activatedRoute: ActivatedRoute,
@@ -49,6 +65,9 @@ export class EstadosComponent {
     this.inventarioService.getCodigo(codigo).subscribe((respo)=>{
       this.cargando=1
       this.descripcion=respo
+      this.formulario.codigo_patrimonial=this.descripcion.codigo_patrimonial
+      this.formulario.estado_bien1=this.descripcion.estado_bien
+      this.formulario.condicion1=this.descripcion.condicion
       console.log(this.descripcion)
     })
   }
@@ -66,14 +85,19 @@ export class EstadosComponent {
       
     } else if (this.selectedOption === 'B') {
       this.inputValue = this.selectedOption.valueOf();
+      this.formulario.estado_bien2=this.selectedOption.valueOf();
     } else if (this.selectedOption === 'R') {
       this.inputValue = this.selectedOption.valueOf();
+      this.formulario.estado_bien2=this.selectedOption.valueOf();
     }else if (this.selectedOption === 'M') {
       this.inputValue = this.selectedOption.valueOf();
+      this.formulario.estado_bien2=this.selectedOption.valueOf();
     }else if (this.selectedOption === 'X') {
       this.inputValue = this.selectedOption.valueOf();
+      this.formulario.estado_bien2=this.selectedOption.valueOf();
     }else if (this.selectedOption === 'Y') {
       this.inputValue = this.selectedOption.valueOf();
+      this.formulario.estado_bien2=this.selectedOption.valueOf();
     }
     console.log(this.inputValue)
   }
@@ -88,10 +112,54 @@ export class EstadosComponent {
     // Por ejemplo:
     if (this.selectedOptionC === 'A') {
       this.inputValueC = this.selectedOptionC.valueOf();
+      this.formulario.condicion2=this.inputValueC;
     } else if (this.selectedOptionC === 'B') {
       this.inputValueC = this.selectedOptionC.valueOf();
+      this.formulario.condicion2=this.inputValueC;
     } 
     console.log(this.inputValueC)
+  }
+  
+
+  enviarFormulario() {
+    console.log(this.formulario)
+    this.formulario.estado_bien2?this.enviarData.estado_bien=this.formulario.estado_bien2:this.enviarData.estado_bien=this.formulario.estado_bien1
+    this.formulario.condicion2?this.enviarData.condicion=this.formulario.condicion2:this.enviarData.condicion=this.formulario.condicion1
+    
+    this.enviarData.codigo_patrimonial=this.formulario.codigo_patrimonial
+    console.log(this.enviarData);
+    swal
+    .fire({
+      title: '¿Estas seguro?',
+      text: `¿Seguro que desea actualizar el bien ${this.formulario.codigo_patrimonial}?`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+    })
+    .then((result) => {
+      if (result.isConfirmed) {
+        this.inventarioService.updateInventario(this.formulario.codigo_patrimonial,this.enviarData)
+      .subscribe((respo)=>{
+        swal.fire(
+          'Actualizando',
+          `El inventario ${this.formulario.codigo_patrimonial} ha sido actualizado`,
+          'success'
+        );
+      })
+        
+        
+      }
+    });
+    // this.inventarioService.updateInventario(this.formulario.codigo_patrimonial,this.enviarData)
+    //   .subscribe((respo)=>{
+    //     console.log(respo)
+    //   })
+    
+  }
+
+  actualizar_inventario(){
+
   }
 }
 
